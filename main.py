eeb9fb8b80d1924a52d46d84f0464c5a1001338e
naru-77 <naru.fumi@icloud.com>
2023-11-11 22:05:25 +0900

    fix

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ from typing import Optional
 
 app = FastAPI()
 
+def fibonacci_tail_helper(a, b, n):
+    if n < 1:
+        return a
+    return fibonacci_tail_helper(b, a + b, n - 1)
+
 def fibonacci(n: int) -> int:
     if n < 0:
         raise ValueError("Negative numbers are not allowed")
     if n > 1000:  # 1000を超える値の場合は計算を制限
         raise ValueError("Value too large")
-    a, b = 0, 1
-    for _ in range(n):
-        a, b = b, a + b
-    return a
+    return fibonacci_tail_helper(0, 1, n)
 
 @app.get("/fib")
 def read_fib(n: Optional[int] = None):
